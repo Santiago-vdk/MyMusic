f61b461a624b94a4fe4bf93622f75a5b9d893f2d
Rafael Chacón Fallas <rafangel1228@gmail.com>
2016-06-03 00:04:36 -0600

    cambios en estructura

---
+++ MyMusic/ODT/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODT
+{
+    public class Class1
+    {
+    }
+}
