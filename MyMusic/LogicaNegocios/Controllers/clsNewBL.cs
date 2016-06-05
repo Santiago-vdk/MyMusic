5207cbe1ce79f5f9caf30d855c4f7669083a71a4
Rafael Chacón Fallas <rafangel1228@gmail.com>
2016-06-04 23:05:19 -0600

    rename

--- MyMusic/LogicaNegocios/Controllers/clsNewBL.cs
+++ MyMusic/LogicaNegocios/Controllers/clsNewBL.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LogicaNegocios.Controllers
+namespace BusinessLogic.Controllers
 {
     public class clsNewBL
     {
