1c97c81a8c92f6bc14ce695d575e301a1d4f8d20
Rafael Chacón Fallas <rafangel1228@gmail.com>
2016-06-04 22:42:00 -0600

    reestructuracion paquete Business Logic

---
+++ MyMusic/LogicaNegocios/FacadeBL.cs
@@ -0,0 +1,20 @@
+﻿using AccesoDatos;
+using DTO;
+using LogicaNegocios.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocios
+{
+   public class FacadeBL
+    {
+        public clsAlbumBL AlbumBL { get; set; }
+
+
+
+
+    }
+}
