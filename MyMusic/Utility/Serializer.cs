4ddafbea5e6a832883cbb620424e7596ee77d017
David <David@DEIVID>
2016-06-06 22:10:40 -0600

    Cambios para diagrama

---
+++ MyMusic/Utility/Serializer.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class Serializer
+    {
+        public Object tojson(Object pObjectObj)
+        {
+            return JsonConvert.SerializeObject(pObjectObj);
+        }    
+    }
+}
