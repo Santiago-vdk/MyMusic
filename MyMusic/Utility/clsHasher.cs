e279a86ca355e8e3ea59e1a7f5771419963b46bf
Rafael Chacón Fallas <rafangel1228@gmail.com>
2016-06-10 23:55:53 -0600

    Implementacion de seguridad

---
+++ MyMusic/Utility/clsHasher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public static class clsHasher
+    {
+        public static string genSalt()
+        {
+            return BCrypt.Net.BCrypt.GenerateSalt(13);
+        }
+
+        public static string hashPassword(string pstringPassword, string pstringSalt)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(pstringPassword, pstringSalt);
+        }
+
+        public static bool compare(string pstringPass1,string pstringPass2)
+        {
+            return (pstringPass1 == pstringPass2);
+        }
+    }
+}
