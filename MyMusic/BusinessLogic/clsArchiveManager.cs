eee0b3cfbdcb4242a30abd63505801863f725512
Rafael Chacón Fallas <rafangel1228@gmail.com>
2016-06-12 15:47:08 -0600

    cambios para imagen

---
+++ MyMusic/BusinessLogic/clsArchiveManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    class clsArchiveManager
+    {
+               
+        public void saveUserImage(string pstringUsername, string pstrtingImage)
+        {
+               /*
+               string contentPath = Server.MapPath("~/Content/albums");
+               string path = Path.Combine(contentPath, album.title.Replace(" ", ""));
+               Directory.CreateDirectory(path);
+               */
+        }
+    }
+}
