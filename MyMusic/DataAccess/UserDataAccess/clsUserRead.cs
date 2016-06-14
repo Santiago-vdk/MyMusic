2783475d1101b06a69c5e30b77b8ade581b9dbb4
David <David@DEIVID>
2016-06-14 00:42:17 -0600

    fix

--- MyMusic/DataAccess/UserDataAccess/clsUserRead.cs
+++ MyMusic/DataAccess/UserDataAccess/clsUserRead.cs
@@ -163,6 +163,7 @@ namespace DataAccess.UserDataAccess
                 pclsInfoUser.SaltHashed = result["HashPassword"].ToString();
                 pclsInfoUser.Salt = result["Salt"].ToString();
                 pclsInfoUser.Id = Convert.ToInt32(result["UserCode"].ToString());
+                pclsInfoUser.Rol = Convert.ToInt32(result["CodeRol"].ToString());
                 pclsResponse.Code = 0;
                 pclsResponse.Message = "Done";
                 pclsResponse.Success = true;
