720cd331179519771b25e227ea6fbd5509e99a05
David <David@DEIVID>
2016-06-18 17:42:15 -0600

    Fix

---
+++ MyMusic/DataAccess/EventsDataAccess/clsEventsRead.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EventsDataAccess
+{
+    public class clsEventsRead
+    {
+        private SqlConnection conn = new clsConnection().getPort();
+
+
+    }
+}
