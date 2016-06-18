037329ba65bea7b5adc910fa9b200e032e93c925
santiago-vdk <santiago.vdk@gmail.com>
2016-06-17 22:35:48 -0600

    Quite el dolor

---
+++ MyMusic/MyFan_Webapp/Areas/Bands/Models/clsAlbum.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFan_Webapp.Areas.Bands.Models
+{
+    public class clsAlbum
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DateCreation { get; set; }
+    }
+}
--- MyMusic/MyFan_Webapp/Areas/Fans/Requests/clsFanRequests.cs
+++ MyMusic/MyFan_Webapp/Areas/Fans/Requests/clsFanRequests.cs
@@ -1,11 +1,7 @@
 ﻿using DTO;
-using MyFan_Webapp.Areas.Fans.Models;
 using MyFan_Webapp.Requests;
-using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Utility;
 
