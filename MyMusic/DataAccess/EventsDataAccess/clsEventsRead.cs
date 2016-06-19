18101bfbd76d2840e4134c82cfa0254d4124da6a
David <David@DEIVID>
2016-06-18 20:55:49 -0600

    Cambios

--- MyMusic/DataAccess/EventsDataAccess/clsEventsRead.cs
+++ MyMusic/DataAccess/EventsDataAccess/clsEventsRead.cs
@@ -49,5 +49,48 @@ namespace DataAccess.EventsDataAccess
             }
 
         }
+        public void geteventreviews(ref List<clsReview> pclsReviews, ref clsResponse pclsResponse, int pintEventCode)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("myFan.SP_GetReviewsEvents", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add("@intCodEvento", System.Data.SqlDbType.Int).Value = pintEventCode;
+                conn.Open();
+                SqlDataReader result = cmd.ExecuteReader();
+
+                List<clsReview> reviews = new List<clsReview>();
+                while (result.Read())
+                {
+                    clsReview tmp = new clsReview();
+                    tmp.Author = (result["strNombre"].ToString());
+                    tmp.Calification = (result["intClasificacion"].ToString());
+                    tmp.Comment = (result["strComentario"].ToString());
+                    reviews.Add(tmp);
+
+                }
+                pclsReviews = reviews;
+                pclsResponse.Code = 0;
+                pclsResponse.Message = "Done";
+                pclsResponse.Success = true;
+            }
+            catch (SqlException ex)
+            {
+                pclsResponse.Code = 1;
+                pclsResponse.Success = false;
+                pclsResponse.Message = "Error while procesing your request.";
+            }
+            catch (Exception ex)
+            {
+                pclsResponse.Code = 2;
+                pclsResponse.Success = false;
+                pclsResponse.Message = "Unexpected error.";
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
     }
 }
