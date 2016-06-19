c5e4f1258279f2a5374b862f9cb641ab98f53e0f
David <David@DEIVID>
2016-06-18 19:49:51 -0600

    Creacion y cambio de estado de eventos

--- MyMusic/DataAccess/EventsDataAccess/clsEventsRead.cs
+++ MyMusic/DataAccess/EventsDataAccess/clsEventsRead.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DTO;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -11,6 +12,42 @@ namespace DataAccess.EventsDataAccess
     {
         private SqlConnection conn = new clsConnection().getPort();
 
+        public void geteventinfo(ref clsEvent pclsEvent, ref clsResponse pclsResponse, int pintDiskCode)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("myFan.SP_GetInfoDisc", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add("@intDisc", System.Data.SqlDbType.Int).Value = pintDiskCode;
+                conn.Open();
+                SqlDataReader result = cmd.ExecuteReader();
 
+                while (result.Read())
+                {
+                   
+                }
+
+                pclsResponse.Code = 0;
+                pclsResponse.Message = "Done";
+                pclsResponse.Success = true;
+            }
+            catch (SqlException ex)
+            {
+                pclsResponse.Code = 1;
+                pclsResponse.Success = false;
+                pclsResponse.Message = "Error while procesing your request.";
+            }
+            catch (Exception ex)
+            {
+                pclsResponse.Code = 2;
+                pclsResponse.Success = false;
+                pclsResponse.Message = "Unexpected error.";
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
     }
 }
